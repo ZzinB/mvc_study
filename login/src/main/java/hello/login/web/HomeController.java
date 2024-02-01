d6182afe9fade804834d41b63e3a3cd993a23be5
ZzinB <mysteleee7@gmail.com>
2024-02-01 17:38:09 +0900

    Fixed: 수정 오류

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,29 +9,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final MemberRepository memberRepository;
-
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
     @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model){
-        if (memberId == null){
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
             return "home";
         }
 
         //로그인
         Member loginMember = memberRepository.findById(memberId);
-        if (memberId == null){
+        if (loginMember == null) {
             return "home";
         }
+
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
