e3dab924d74944bdb4d405209a58b371eb8e781e
ZzinB <mysteleee7@gmail.com>
2024-02-01 17:43:54 +0900

    Add: 추가 로그아웃

--- login/src/main/java/hello/login/domain/login/LoginController.java
+++ login/src/main/java/hello/login/domain/login/LoginController.java
@@ -43,5 +43,18 @@ public class LoginController {
         Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
         response.addCookie(idCookie);
         return "redirect:/";
+
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
