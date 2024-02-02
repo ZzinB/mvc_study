289211d7ff1c0963104375dcfeac35af04ad0b8a
ZzinB <mysteleee7@gmail.com>
2024-02-02 11:54:12 +0900

    Refac: 리팩토링 세션직접만들 - 쿠키찾기

--- login/src/main/java/hello/login/web/session/SessionManager.java
+++ login/src/main/java/hello/login/web/session/SessionManager.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -41,15 +42,20 @@ public class SessionManager {
      * 세션 조회
      */
     public Object getSession(HttpServletRequest request){
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null){
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null){
             return null;
         }
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals(SESSION_COOKIE_NAME)){
-                return sessionStore.get(cookie.getValue());
-            }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    public Cookie findCookie(HttpServletRequest request, String cookieName){
+        if (request.getCookies() == null){
+            return null;
         }
-        return null;
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
     }
 }
