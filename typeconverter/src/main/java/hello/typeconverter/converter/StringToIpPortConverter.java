80a753cc0616c1ca8f2755b18fc84c8cc708932b
ZzinB <mysteleee7@gmail.com>
2024-02-09 17:01:21 +0900

    Comment: 디버그주석추가

--- typeconverter/src/main/java/hello/typeconverter/converter/StringToIpPortConverter.java
+++ typeconverter/src/main/java/hello/typeconverter/converter/StringToIpPortConverter.java
@@ -9,6 +9,7 @@ public class StringToIpPortConverter implements Converter<String, IpPort> {
     @Override
     public IpPort convert(String source) {
         log.info("convert source={}", source);
+        //"127.0.0.1:8000" -> IpPort 객체
         String[] split = source.split(":");
         String ip = split[0];
         int port = Integer.parseInt(split[1]);
